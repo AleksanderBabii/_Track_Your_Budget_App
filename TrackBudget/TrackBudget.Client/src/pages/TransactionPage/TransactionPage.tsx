01d378b78e7848db00556390983f53777d0399a9
LexRift <82161795+AleksanderBabii@users.noreply.github.com>
2026-08-05 21:40:14 +0200

    feat: Add useMemo import to TransactionPage
    Import the useMemo hook from React for performance optimization in TransactionPage component.

--- TrackBudget/TrackBudget.Client/src/pages/TransactionPage/TransactionPage.tsx
+++ TrackBudget/TrackBudget.Client/src/pages/TransactionPage/TransactionPage.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useMemo } from "react";
 
 import { Button } from "../../components/common/Button/Button";
 import { EmptyState } from "../../components/common/EmptyState/EmptyState";
